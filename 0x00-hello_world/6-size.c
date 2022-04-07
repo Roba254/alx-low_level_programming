e71841475c056d9c733b82ebd3006e3b32cc5fa7
Roba254 <robertkimutai4@gmail.com>
2022-04-07 09:31:00 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main() {
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %zu bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
-    
+
     return 0;
 }
+
