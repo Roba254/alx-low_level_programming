d6dbbd248432b21550b423feefcc39997669c5af
Roba254 <robertkimutai4@gmail.com>
2022-04-07 08:01:35 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#include <stdio>
+#include <stdio.h>
 
-
-int main (void){
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+int main(void){
+  printf("Programming is like building a multilingual puzzle\n");
+  return 0;
 }
